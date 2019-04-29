ad61de6264279e94516384ff2927d1148efff533
Tezin Vladimir <vtezin@gmail.com>
2019-04-29 07:41:38 +0300

    Initial Commit

---
+++ C3. L2. View controllers data exchange/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  C3. L2. View controllers data exchange
+//
+//  Created by Admin on 29/04/2019.
+//  Copyright © 2019 Vladimir Tezin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
